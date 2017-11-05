bef8a3a0e907d87848588275be8854dc31b0f23c
Italo Lanza <itmilhomem@gmail.com>
2017-11-05 19:23:36 -0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@
 import redis
 import os
 import telebot
+import json
+from time import localtime,strftime
+from pprint import pprint
 # import some_api_lib
 # import ...
 
@@ -15,6 +18,32 @@ some_api_token = os.environ['SOME_API_TOKEN']
 r = redis.from_url(os.environ.get("REDIS_URL"))
 
 #       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+bot = telebot.TeleBot(token);
+
+#funcao que é chamada quanto o texto da mensagem comeca com '\start'
+@bot.message_handler(commands=['start','hello'])
+def command_start(message):
+	#jsonMensagem 	= json.loads(message)
+	#textoResposta 	= ''.join('Ola', jsonMensagem['message']['user']['first_name'])
+	bot.reply_to(message, 'Ola '+message.from_user.first_name+', esta e a mensagem de boas vindas.')
+	pass
+#funcao que é chamada quanto o texto da mensagem comeca com '\time'
+@bot.message_handler(commands=['time'])
+def command_time(message):
+	bot.reply_to(message, strftime("%d/%m/%Y -  %H:%M (%A)",localtime()))
+	pass
+
+@bot.message_handler(commands=['universo'])
+def command_time(message):
+	bot.send_message(message.chat.id, '42 é a resposta para a vida, o universo e tudo mais.')
+	pass
+
+#funcao que é chamada quanto o texto da mensagem comeca com for diferente dos comandos especificados
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.send_message(message.chat.id, 'Mensagem recebida: '+'"'+message.text+'"')
+    pass
+
+
+#faz com que o bot fique procurando por atualizações no servidor do telegram
+bot.polling()          ...
